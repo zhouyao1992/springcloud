769db6c7279a23293e67c0195f2243f114f1faa8
zhouy <412729287@qq.com>
2018-04-12 16:18:39 +0800

    feign消费者

--- feign-consumer/src/main/java/com/zhou/controller/FeignConsumerController.java
+++ feign-consumer/src/main/java/com/zhou/controller/FeignConsumerController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
+ * feign消费者
  * Created by zhouyao on 2018/4/12.
  */
 @RestController
