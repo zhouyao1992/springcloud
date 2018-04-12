5d1a44a6c0c9339ea208b191da89efb05efdd4fc
zhouy <412729287@qq.com>
2018-04-12 16:17:34 +0800

    ribbon消费者

--- ribbon-consumer/src/main/java/com/zhou/controller/RibbonConsumerController.java
+++ ribbon-consumer/src/main/java/com/zhou/controller/RibbonConsumerController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 /**
+ * ribbon消费者
  * Created by zhouyao on 2018/4/9.
  */
 @RestController
